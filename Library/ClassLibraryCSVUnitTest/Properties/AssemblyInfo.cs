90ea4ade32b759d32da09acd61aa0364bab236e1
Raphael Nöldner <r_n@gmx.net>
2018-01-23 20:10:23 +0100

    Cleanup of AssemblyInfo

--- Library/ClassLibraryCSVUnitTest/Properties/AssemblyInfo.cs
+++ Library/ClassLibraryCSVUnitTest/Properties/AssemblyInfo.cs
@@ -4,14 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CvsToolLib UnitTest")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Cornerstone OnDemand, Inc.")]
-[assembly: AssemblyProduct("CvsToolLib UnitTest")]
-[assembly: AssemblyCopyright("Copyright © Cornerstone OnDemand, Inc. 2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("ClassLibraryCSVUnitTest")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -20,16 +13,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("131d098a-d627-4615-8110-e92b55070612")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
