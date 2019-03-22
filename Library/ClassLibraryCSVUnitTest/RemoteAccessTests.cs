5f18fde46f78c3119e5c828e19cc7f1cee2a5a96
RNoeldner <r_n@gmx.net>
2019-03-22 18:13:53 +0100

    Moving in RemoteAccess from other project Fix bug with measuring field size if log columns have been read for determining number of column

---
+++ Library/ClassLibraryCSVUnitTest/RemoteAccessTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CsvTools.Tests
+{
+  [TestClass()]
+  public class RemoteAccessTests
+  {
+    public void CopyToTest()
+    {
+      // Covered by generic test
+    }
+
+    public void EqualsTest()
+    {
+      // Covered by generic test
+    }
+
+    [TestMethod()]
+    public void RemoteAccessTest()
+    {
+      var testCase = new RemoteAccess();
+      bool hasFired = false;
+      testCase.PropertyChanged += delegate (object sender, System.ComponentModel.PropertyChangedEventArgs e)
+      {
+        hasFired = true;
+      };
+      testCase.HostName = "Test";
+      Assert.AreEqual("Test", testCase.HostName);
+      Assert.IsTrue(hasFired);
+      hasFired = false;
+      testCase.HostName = "Test";
+      Assert.IsFalse(hasFired);
+
+      testCase.User = "Hello";
+      Assert.IsTrue(hasFired);
+      Assert.AreEqual("Hello", testCase.User);
+      hasFired = false;
+      testCase.User = "Hello";
+      Assert.IsFalse(hasFired);
+
+      testCase.Password = "World";
+      Assert.IsTrue(hasFired);
+      Assert.AreEqual("World", testCase.Password);
+      hasFired = false;
+      testCase.Password = "World";
+      Assert.IsFalse(hasFired);
+    }
+  }
+}
