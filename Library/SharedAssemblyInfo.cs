2a804e0edcfd421fc428739f3e6c57ae532b010e
RNoeldner <r_n@gmx.net>
2019-05-06 18:39:05 +0200

    Fixed an error with empty DataSoucres in Grouped ComboBox, still UI is broken once as soon as filter emptied out data source. Change error message in BaseFileReader.HandleRemoteFile RemotePth is not the right information

--- Library/SharedAssemblyInfo.cs
+++ Library/SharedAssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.0.343")]
-[assembly: AssemblyFileVersion("1.6.0.343")]
-[assembly: AssemblyInformationalVersion("1.6.0.343")] // a.k.a. "Product version"
+[assembly: AssemblyVersion("1.6.0.344")]
+[assembly: AssemblyFileVersion("1.6.0.344")]
+[assembly: AssemblyInformationalVersion("1.6.0.344")] // a.k.a. "Product version"
