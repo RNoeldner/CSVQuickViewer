44fbc1afaef7a7d0e1b79cbc79e14c2773efaf85
RNoeldner <r_n@gmx.net>
2019-06-12 08:30:05 +0200

    Fixed an issue in Detail view, some functionality not working as soon as no record is shown after filter. Contentments did not know which column they are in.

--- Library/SharedAssemblyInfo.cs
+++ Library/SharedAssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.2.358")]
-[assembly: AssemblyFileVersion("1.6.2.358")]
-[assembly: AssemblyInformationalVersion("1.6.2.358")] // a.k.a. "Product version"
+[assembly: AssemblyVersion("1.6.2.359")]
+[assembly: AssemblyFileVersion("1.6.2.359")]
+[assembly: AssemblyInformationalVersion("1.6.2.359")] // a.k.a. "Product version"
