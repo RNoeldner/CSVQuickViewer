446053b0f90889a0bda4535764693ae8a1d8c5b0
Raphael Noeldner <r_n@gmx.net>
2019-10-08 09:31:44 +0200

    Added class to be able to distigish bween hidden and visiable sheets.

---
+++ Library/ClassLibraryCSV/Sheet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace CsvTools
+{
+  public class Sheet
+  {
+    public readonly string Name;
+    public readonly bool Hidden;
+    
+    public Sheet(string name, bool hidden)
+    {
+      if (string.IsNullOrWhiteSpace(name))
+        throw new ArgumentException("Name of sheet can not be empty", nameof(name));
+      Name = name;
+      Hidden = hidden;
+    }
+
+    public override bool Equals(object obj) => obj is Sheet sheets && Name == sheets.Name && Hidden == sheets.Hidden;
+
+    public override int GetHashCode()
+    {
+      var hashCode = 1548707919;
+      hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
+      hashCode = hashCode * -1521134295 + Hidden.GetHashCode();
+      return hashCode;
+    }
+
+    public override string ToString() => (Hidden ? "(" : string.Empty) + Name + (Hidden ? ")*" : string.Empty);
+  }
+}
