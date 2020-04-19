302a26c2def710ee7b99134101845e199a3121de
Raphael Noeldner <r_n@gmx.net>
2020-04-19 22:01:07 +0200

    Fixed DataTableReaderTests

--- UnitTest/ClassLibraryCSVUnitTest/DataTableReaderTests.cs
+++ UnitTest/ClassLibraryCSVUnitTest/DataTableReaderTests.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using CsvTools;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CsvTools.Tests
@@ -15,31 +11,7 @@ namespace CsvTools.Tests
   [TestClass()]
   public class DataTableReaderTests
   {
-    private DataTable m_DataTable = RandomDataTable(100);
-
-    private static DataTable RandomDataTable(int recs)
-    {
-      var dataTable = new DataTable { TableName = "DataTable", Locale = CultureInfo.InvariantCulture })
-      
-      dataTable.Columns.Add("ID", typeof(int));
-      dataTable.Columns.Add("Text", typeof(string));
-      dataTable.Columns.Add("ColText1", typeof(string));
-      dataTable.Columns.Add("ColText2", typeof(string));
-      dataTable.Columns.Add("ColTextDT", typeof(DateTime));
-      var random = new Random(new Guid().GetHashCode());
-      for (var i = 0; i < recs; i++)
-      {
-        var row = dataTable.NewRow();
-        row["ID"] = i;
-        row["Text"] = i.ToString(CultureInfo.CurrentCulture);
-        row["ColText1"] = $"Test{i + 1}";
-        row["ColText2"] = $"Text {i * 2} !";
-        row["ColTextDT"] = new DateTime(random.Next(1900, 2030), random.Next(1, 12), 1).AddDays(random.Next(1, 31));
-        dataTable.Rows.Add(row);
-      }
-
-      return dataTable;
-    }
+    private readonly DataTable m_DataTable = RandomDataTable(100);
 
     [TestMethod()]
     public void DataTableReaderTest()
@@ -53,28 +25,6 @@ namespace CsvTools.Tests
           Assert.IsFalse(test.IsClosed);
         }
       }
-    }
-
-    [TestMethod()]
-    public void GetDataTableTest()
-    {
-      using (var pd = new DummyProcessDisplay())
-      {
-        try
-        {
-          using (var test = new DataTableReader(null, "id", pd))
-          {
-          }
-        }
-        catch (ArgumentNullException e)
-        {
-        }
-        catch (Exception ex)
-        {
-          Assert.Fail("Wrong Exception Type: " + ex.GetType());
-        }
-      }
-
     }
 
     [TestMethod()]
@@ -92,14 +42,22 @@ namespace CsvTools.Tests
     }
 
     [TestMethod()]
-    public void GetNameTest()
+    public void GetDataTableTest()
     {
       using (var pd = new DummyProcessDisplay())
       {
-        using (var test = new DataTableReader(m_DataTable, "id", pd))
+        try
         {
-          test.Open();
-          Assert.AreEqual("ID", test.GetName(0));
+          using (var test = new DataTableReader(null, "id", pd))
+          {
+          }
+        }
+        catch (ArgumentNullException)
+        {
+        }
+        catch (Exception ex)
+        {
+          Assert.Fail("Wrong Exception Type: " + ex.GetType());
         }
       }
     }
@@ -130,7 +88,18 @@ namespace CsvTools.Tests
       }
     }
 
-  
+    [TestMethod()]
+    public void GetNameTest()
+    {
+      using (var pd = new DummyProcessDisplay())
+      {
+        using (var test = new DataTableReader(m_DataTable, "id", pd))
+        {
+          test.Open();
+          Assert.AreEqual("ID", test.GetName(0));
+        }
+      }
+    }
 
     [TestMethod()]
     public void GetOrdinalTest()
@@ -141,22 +110,6 @@ namespace CsvTools.Tests
         {
           test.Open();
           Assert.AreEqual(2, test.GetOrdinal("ColText1"));
-        }
-      }
-
-    }
-
-    
-
-    [TestMethod()]
-    public void ReadTest()
-    {
-      using (var pd = new DummyProcessDisplay())
-      {
-        using (var test = new DataTableReader(m_DataTable, "id", pd))
-        {
-          test.Open();
-          Assert.IsTrue(test.Read());
         }
       }
     }
@@ -172,6 +125,43 @@ namespace CsvTools.Tests
           Assert.IsTrue(await test.ReadAsync());
         }
       }
+    }
+
+    [TestMethod()]
+    public void ReadTest()
+    {
+      using (var pd = new DummyProcessDisplay())
+      {
+        using (var test = new DataTableReader(m_DataTable, "id", pd))
+        {
+          test.Open();
+          Assert.IsTrue(test.Read());
+        }
+      }
+    }
+
+    private static DataTable RandomDataTable(int recs)
+    {
+      var dataTable = new DataTable { TableName = "DataTable", Locale = CultureInfo.InvariantCulture };
+
+      dataTable.Columns.Add("ID", typeof(int));
+      dataTable.Columns.Add("Text", typeof(string));
+      dataTable.Columns.Add("ColText1", typeof(string));
+      dataTable.Columns.Add("ColText2", typeof(string));
+      dataTable.Columns.Add("ColTextDT", typeof(DateTime));
+      var random = new Random(new Guid().GetHashCode());
+      for (var i = 0; i < recs; i++)
+      {
+        var row = dataTable.NewRow();
+        row["ID"] = i;
+        row["Text"] = i.ToString(CultureInfo.CurrentCulture);
+        row["ColText1"] = $"Test{i + 1}";
+        row["ColText2"] = $"Text {i * 2} !";
+        row["ColTextDT"] = new DateTime(random.Next(1900, 2030), random.Next(1, 12), 1).AddDays(random.Next(1, 31));
+        dataTable.Rows.Add(row);
+      }
+
+      return dataTable;
     }
   }
 }
