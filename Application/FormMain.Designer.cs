3f2440aabb07fbfad6258cb145263e606341564e
Raphael Noeldner <r_n@gmx.net>
2020-09-01 09:01:45 +0200

    Making Read Async in FormCsvTextDisplay even though it does not support cancellation FormCsvTextDisplay is opened non modal

--- Application/FormMain.Designer.cs
+++ Application/FormMain.Designer.cs
@@ -22,7 +22,8 @@
         DataTable?.Dispose();
         m_CancellationTokenSource?.Dispose();       
         m_SettingsChangedTimerChange?.Dispose();
-      }
+        m_SourceDisplay?.Dispose();
+      }      
       Microsoft.Win32.SystemEvents.DisplaySettingsChanged -= SystemEvents_DisplaySettingsChanged;
       Microsoft.Win32.SystemEvents.PowerModeChanged -= SystemEvents_PowerModeChanged;
       base.Dispose(disposing);
