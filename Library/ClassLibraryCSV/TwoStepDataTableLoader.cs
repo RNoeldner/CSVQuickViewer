73aa51cd509260e307414099f53bdf40285f34f0
Raphael Noeldner <r_n@gmx.net>
2020-10-28 12:10:22 +0100

    Split DetailControlLoader in two classes one without UI interaction one with

---
+++ Library/ClassLibraryCSV/TwoStepDataTableLoader.cs
@@ -0,0 +1,105 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Data;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CsvTools
+{
+  public class TwoStepDataTableLoader : IDisposable
+  {
+    [CanBeNull] private readonly Action m_ActionBegin;
+    [CanBeNull] private readonly Action<DataReaderWrapper> m_ActionFinished;
+    [NotNull] private readonly Func<DataTable> m_GetDataTable;
+    [CanBeNull] private readonly Func<FilterType, CancellationToken, Task> m_RefreshDisplayAsync;
+    [NotNull] private readonly Action<DataTable> m_SetDataTable;
+    [CanBeNull] private readonly Action<Func<IProcessDisplay, Task>> m_SetLoadNextBatchAsync;
+    [CanBeNull] private DataReaderWrapper m_DataReaderWrapper;
+    [CanBeNull] private IFileReader m_FileReader;
+
+    protected TwoStepDataTableLoader([NotNull] Action<DataTable> actionSetDataTable,
+      [NotNull] Func<DataTable> getDataTable,
+      [CanBeNull] Func<FilterType, CancellationToken, Task> setRefreshDisplayAsync,
+      [CanBeNull] Action<Func<IProcessDisplay, Task>> loadNextBatchAsync, [CanBeNull] Action actionBegin,
+      [CanBeNull] Action<DataReaderWrapper> actionFinished)
+    {
+      m_SetDataTable = actionSetDataTable ?? throw new ArgumentNullException(nameof(actionSetDataTable));
+      m_GetDataTable = getDataTable ?? throw new ArgumentNullException(nameof(getDataTable));
+      m_SetLoadNextBatchAsync = loadNextBatchAsync;
+      m_RefreshDisplayAsync = setRefreshDisplayAsync;
+      m_ActionFinished = actionFinished;
+      m_ActionBegin = actionBegin;
+    }
+
+    public void Dispose()
+    {
+      m_DataReaderWrapper?.Dispose();
+      m_DataReaderWrapper = null;
+      m_FileReader?.Dispose();
+      m_FileReader = null;
+    }
+
+    public async Task StartAsync([NotNull] IFileSetting fileSetting, bool includeError, TimeSpan durationInitial,
+      [NotNull] IProcessDisplay processDisplay, [CanBeNull] EventHandler<WarningEventArgs> addWarning)
+    {
+      m_FileReader = FunctionalDI.GetFileReader(fileSetting, TimeZoneInfo.Local.Id, processDisplay);
+      if (m_FileReader == null)
+        throw new FileReaderException($"Could not get reader for {fileSetting}");
+
+      RowErrorCollection warningList = null;
+      if (addWarning != null)
+      {
+        warningList = new RowErrorCollection(m_FileReader);
+        m_FileReader.Warning += addWarning;
+        m_FileReader.Warning -= warningList.Add;
+      }
+
+      await m_FileReader.OpenAsync(processDisplay.CancellationToken).ConfigureAwait(false);
+
+      if (addWarning != null)
+      {
+        warningList.HandleIgnoredColumns(m_FileReader);
+        warningList.PassWarning += addWarning;
+      }
+
+      m_DataReaderWrapper = new DataReaderWrapper(m_FileReader, fileSetting.RecordLimit, includeError,
+        fileSetting.DisplayStartLineNo, fileSetting.DisplayRecordNo, fileSetting.DisplayEndLineNo);
+
+      m_SetDataTable(m_DataReaderWrapper.GetEmptyDataTable());
+      m_ActionBegin?.Invoke();
+
+      await GetBatchByTimeSpan(durationInitial, includeError, processDisplay).ConfigureAwait(false);
+
+      m_SetLoadNextBatchAsync?.Invoke(process => GetBatchByTimeSpan(TimeSpan.MaxValue, includeError, process));
+
+      m_ActionFinished?.Invoke(m_DataReaderWrapper);
+    }
+
+
+    private async Task GetBatchByTimeSpan(TimeSpan maxDuration, bool restoreError,
+      [NotNull] IProcessDisplay processDisplay)
+    {
+      if (m_DataReaderWrapper == null)
+        return;
+      if (processDisplay == null)
+        throw new ArgumentNullException(nameof(processDisplay));
+      try
+      {
+        processDisplay.SetMaximum(-1);
+        await m_DataReaderWrapper.LoadDataTable(m_GetDataTable(), maxDuration, restoreError,
+          (l, i) => processDisplay.SetProcess($"Reading data...\nRecord: {l:N0}", i, false),
+          processDisplay.CancellationToken).ConfigureAwait(false);
+
+        await m_RefreshDisplayAsync(FilterType.All, processDisplay.CancellationToken)
+          .ConfigureAwait(false);
+
+        if (m_DataReaderWrapper.EndOfFile)
+          Dispose();
+      }
+      catch (Exception e)
+      {
+        Logger.Error(e);
+      }
+    }
+  }
+}
--- Library/WinFormControls/DetailControlLoader.cs
+++ Library/WinFormControls/DetailControlLoader.cs
@@ -1,108 +1,21 @@
-﻿using JetBrains.Annotations;
-using System;
-using System.Data;
-using System.Threading.Tasks;
-
-namespace CsvTools
+﻿namespace CsvTools
 {
-  public class DetailControlLoader : IDisposable
+  public class DetailControlLoader : TwoStepDataTableLoader
   {
-    private readonly DetailControl m_DetailControl;
-    private DataReaderWrapper m_DataReaderWrapper;
-    private IFileReader m_FileReader;
-    private bool m_RestoreError;
-
-    public DetailControlLoader(DetailControl detailControl) => m_DetailControl = detailControl;
-
-    public void Dispose()
-    {
-      m_DataReaderWrapper?.Dispose();
-      m_DataReaderWrapper = null;
-      m_FileReader?.Dispose();
-      m_FileReader=null;
-    }
-
-    public async Task StartAsync([NotNull] IFileSetting fileSetting, bool includeError, TimeSpan durationInitial,
-      [NotNull] IProcessDisplay processDisplay, [CanBeNull] EventHandler<WarningEventArgs> addWarning)
-    {
-      m_FileReader = FunctionalDI.GetFileReader(fileSetting, TimeZoneInfo.Local.Id, processDisplay);
-
-      m_RestoreError = includeError;
-      RowErrorCollection warningList = null;
-      if (addWarning != null)
+    public DetailControlLoader(DetailControl detailControl) : base(
+      table => detailControl.DataTable = table,
+      () => detailControl.DataTable, detailControl.RefreshDisplayAsync,
+      func => detailControl.LoadNextBatchAsync = func,
+      () => detailControl.toolStripButtonNext.Enabled = false,
+      wrapper =>
       {
-        warningList = new RowErrorCollection(m_FileReader);
-        m_FileReader.Warning += addWarning;
-        m_FileReader.Warning -= warningList.Add;
-      }
+        detailControl.EndOfFile = () =>
+          wrapper?.EndOfFile ?? true;
 
-      await m_FileReader.OpenAsync(processDisplay.CancellationToken).ConfigureAwait(false);
-
-      if (addWarning != null)
-      {
-        warningList.HandleIgnoredColumns(m_FileReader);
-        warningList.PassWarning += addWarning;
-      }
-
-      m_DataReaderWrapper = new DataReaderWrapper(m_FileReader, fileSetting.RecordLimit, includeError,
-        fileSetting.DisplayStartLineNo, fileSetting.DisplayRecordNo, fileSetting.DisplayEndLineNo);
-
-      m_DetailControl.DataTable = m_DataReaderWrapper.GetEmptyDataTable();
-
-      m_DetailControl.toolStripButtonNext.Enabled = false;
-      await NextBatch(durationInitial, processDisplay).ConfigureAwait(false);
-
-      m_DetailControl.LoadNextBatchAsync = process => NextBatch(TimeSpan.MaxValue, process);
-      m_DetailControl.EndOfFile = () =>
-        m_DataReaderWrapper?.EndOfFile ?? true;
-
-      m_DetailControl.toolStripButtonNext.Visible = m_DataReaderWrapper != null;
-      m_DetailControl.toolStripButtonNext.Enabled = m_DataReaderWrapper != null;
-
-    }
-
-    public async Task StartAsync([NotNull] IDataReader dataReader, TimeSpan durationInitial,
-          [NotNull] IProcessDisplay processDisplay)
+        detailControl.toolStripButtonNext.Visible = wrapper != null;
+        detailControl.toolStripButtonNext.Enabled = wrapper != null;
+      })
     {
-      m_RestoreError=true;
-      m_DataReaderWrapper = new DataReaderWrapper(dataReader, dataReader.RecordsAffected, false, false, false, false);
-
-      m_DetailControl.DataTable = m_DataReaderWrapper.GetEmptyDataTable();
-
-      m_DetailControl.toolStripButtonNext.Enabled = false;
-      await NextBatch(durationInitial, processDisplay).ConfigureAwait(false);
-
-      m_DetailControl.LoadNextBatchAsync = process => NextBatch(TimeSpan.MaxValue, process);
-      m_DetailControl.EndOfFile = () =>
-        m_DataReaderWrapper?.EndOfFile ?? true;
-
-      m_DetailControl.toolStripButtonNext.Visible = m_DataReaderWrapper != null;
-      m_DetailControl.toolStripButtonNext.Enabled = m_DataReaderWrapper != null;
-    }
-
-    private async Task NextBatch(TimeSpan maxDuration, [NotNull] IProcessDisplay processDisplay)
-    {
-      if (m_DataReaderWrapper == null)
-        return;
-      if (processDisplay == null)
-        throw new ArgumentNullException(nameof(processDisplay));
-      try
-      {
-        processDisplay.SetMaximum(-1);
-        await m_DataReaderWrapper.LoadDataTable(m_DetailControl.DataTable, maxDuration, m_RestoreError,
-          (l, i) => processDisplay.SetProcess($"Reading data...\nRecord: {l:N0}", i, false),
-          processDisplay.CancellationToken).ConfigureAwait(false);
-
-        await m_DetailControl.RefreshDisplayAsync(FilterType.All, processDisplay.CancellationToken)
-          .ConfigureAwait(false);
-
-        if (m_DataReaderWrapper.EndOfFile)
-          Dispose();
-      }
-      catch (Exception e)
-      {
-        Logger.Error(e);
-      }
     }
   }
 }
