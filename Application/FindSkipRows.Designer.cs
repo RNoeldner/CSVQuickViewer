7dde77dfc0bbbf02f7415a8070910add12676a97
Raphael Noeldner <r_n@gmx.net>
2021-06-18 13:02:02 +0200

    Added FastColoredTextBox as DLL, instead of Project Refreence RemovedResources for ServiceColors (again)

--- Application/FindSkipRows.Designer.cs
+++ Application/FindSkipRows.Designer.cs
@@ -197,8 +197,7 @@
 			this.textBox.Name = "textBox";
 			this.textBox.Paddings = new System.Windows.Forms.Padding(0);
 			this.textBox.ReadOnly = true;
-			this.textBox.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
-			this.textBox.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("textBox.ServiceColors")));
+			this.textBox.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));			
 			this.textBox.Size = new System.Drawing.Size(609, 544);
 			this.textBox.TabIndex = 126;
 			this.textBox.WordWrap = true;
--- Application/FormCsvTextDisplay.Designer.cs
+++ Application/FormCsvTextDisplay.Designer.cs
@@ -74,8 +74,7 @@ namespace CsvTools
 			this.textBox.Name = "textBox";
 			this.textBox.Paddings = new System.Windows.Forms.Padding(0);
 			this.textBox.ReadOnly = true;
-			this.textBox.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
-			this.textBox.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("textBox.ServiceColors")));
+			this.textBox.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));			
 			this.textBox.ShowFoldingLines = true;
 			this.textBox.Size = new System.Drawing.Size(718, 362);
 			this.textBox.TabIndex = 1;
