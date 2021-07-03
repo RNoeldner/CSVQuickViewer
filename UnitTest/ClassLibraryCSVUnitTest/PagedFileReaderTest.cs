2c1ecc5ae272edf478d2ce627ea688eae27235d0
Raphael Noeldner <r_n@gmx.net>
2021-07-03 17:03:49 +0200

    Made Maifest date readonly

--- UnitTest/ClassLibraryCSVUnitTest/PagedFileReaderTest.cs
+++ UnitTest/ClassLibraryCSVUnitTest/PagedFileReaderTest.cs
@@ -1,18 +1,74 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Dynamic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CsvTools.Tests
 {
-
   [TestClass()]
   public class PagedFileReaderTest
   {
     private CsvFile setting = UnitTestHelper.ReaderGetAllFormats();
+
+    [TestMethod()]
+    public async Task MoveToLastPageFirstPageAsync()
+    {
+      int pageSize = 20;
+      int numRec = 1065;
+      // 1065 / 17
+      using (var process = new CustomProcessDisplay(UnitTestInitializeCsv.Token))
+      using (var reader = new CsvFileReader(setting.FullPath, setting.CodePageId, setting.SkipRows, setting.HasFieldHeader, setting.ColumnCollection, setting.TrimmingOption, setting.FileFormat.FieldDelimiter, setting.FileFormat.FieldQualifier, setting.FileFormat.EscapeCharacter, setting.RecordLimit, setting.AllowRowCombining, setting.FileFormat.AlternateQuoting, setting.FileFormat.CommentLine, setting.NumWarnings, setting.FileFormat.DuplicateQuotingToEscape, setting.FileFormat.NewLinePlaceholder, setting.FileFormat.DelimiterPlaceholder, setting.FileFormat.QuotePlaceholder, setting.SkipDuplicateHeader, setting.TreatLFAsSpace, setting.TreatUnknownCharacterAsSpace, setting.TryToSolveMoreColumns, setting.WarnDelimiterInValue, setting.WarnLineFeed, setting.WarnNBSP, setting.WarnQuotes, setting.WarnUnknownCharacter, setting.WarnEmptyTailingColumns, setting.TreatNBSPAsSpace, setting.TreatTextAsNull, setting.SkipEmptyLines, setting.ConsecutiveEmptyRows, setting.IdentifierInContainer, process))
+      {
+        var test = new PagedFileReader(reader, pageSize, UnitTestInitializeCsv.Token);
+        await test.OpenAsync(true, true, true, true);
+
+        await test.MoveToLastPageAsync();
+        Assert.AreEqual(numRec /pageSize + (numRec % pageSize ==0 ? 0 : 1), test.PageIndex);
+        Assert.AreEqual(numRec % pageSize, test.Count);
+
+        await test.MoveToFirstPageAsync();
+        Assert.AreEqual(pageSize, test.Count);
+      }
+    }
+
+    [TestMethod()]
+    public async Task MoveToNextPageAsync()
+    {
+      int pageSize = 20;
+      int numRec = 1065;
+      // 1065 / 17
+      using (var process = new CustomProcessDisplay(UnitTestInitializeCsv.Token))
+      using (var reader = new CsvFileReader(setting.FullPath, setting.CodePageId, setting.SkipRows, setting.HasFieldHeader, setting.ColumnCollection, setting.TrimmingOption, setting.FileFormat.FieldDelimiter, setting.FileFormat.FieldQualifier, setting.FileFormat.EscapeCharacter, setting.RecordLimit, setting.AllowRowCombining, setting.FileFormat.AlternateQuoting, setting.FileFormat.CommentLine, setting.NumWarnings, setting.FileFormat.DuplicateQuotingToEscape, setting.FileFormat.NewLinePlaceholder, setting.FileFormat.DelimiterPlaceholder, setting.FileFormat.QuotePlaceholder, setting.SkipDuplicateHeader, setting.TreatLFAsSpace, setting.TreatUnknownCharacterAsSpace, setting.TryToSolveMoreColumns, setting.WarnDelimiterInValue, setting.WarnLineFeed, setting.WarnNBSP, setting.WarnQuotes, setting.WarnUnknownCharacter, setting.WarnEmptyTailingColumns, setting.TreatNBSPAsSpace, setting.TreatTextAsNull, setting.SkipEmptyLines, setting.ConsecutiveEmptyRows, setting.IdentifierInContainer, process))
+      {
+        var test = new PagedFileReader(reader, pageSize, UnitTestInitializeCsv.Token);
+        await test.OpenAsync(true, true, true, true);
+        Assert.AreEqual(pageSize, test.Count);
+
+        bool collectionChangedCalled = false;
+        test.CollectionChanged += (o, s) => { collectionChangedCalled = true; };
+        await test.MoveToNextPageAsync();
+        Assert.IsTrue(collectionChangedCalled);
+        Assert.AreEqual(pageSize, test.Count);
+      }
+    }
+
+    [TestMethod()]
+    public async Task MoveToPreviousPageAsync()
+    {
+      int pageSize = 20;
+      // 1065 / 17
+      using (var process = new CustomProcessDisplay(UnitTestInitializeCsv.Token))
+      using (var reader = new CsvFileReader(setting.FullPath, setting.CodePageId, setting.SkipRows, setting.HasFieldHeader, setting.ColumnCollection, setting.TrimmingOption, setting.FileFormat.FieldDelimiter, setting.FileFormat.FieldQualifier, setting.FileFormat.EscapeCharacter, setting.RecordLimit, setting.AllowRowCombining, setting.FileFormat.AlternateQuoting, setting.FileFormat.CommentLine, setting.NumWarnings, setting.FileFormat.DuplicateQuotingToEscape, setting.FileFormat.NewLinePlaceholder, setting.FileFormat.DelimiterPlaceholder, setting.FileFormat.QuotePlaceholder, setting.SkipDuplicateHeader, setting.TreatLFAsSpace, setting.TreatUnknownCharacterAsSpace, setting.TryToSolveMoreColumns, setting.WarnDelimiterInValue, setting.WarnLineFeed, setting.WarnNBSP, setting.WarnQuotes, setting.WarnUnknownCharacter, setting.WarnEmptyTailingColumns, setting.TreatNBSPAsSpace, setting.TreatTextAsNull, setting.SkipEmptyLines, setting.ConsecutiveEmptyRows, setting.IdentifierInContainer, process))
+      {
+        var test = new PagedFileReader(reader, pageSize, UnitTestInitializeCsv.Token);
+        await test.OpenAsync(true, true, true, true);
+        Assert.AreEqual(pageSize, test.Count);
+
+        await test.MoveToNextPageAsync();
+        Assert.AreEqual(pageSize, test.Count);
+        await test.MoveToPreviousPageAsync();
+        Assert.AreEqual(pageSize, test.Count);
+      }
+    }
+
     [TestMethod()]
     public async Task PagedFileReaderOpen()
     {
@@ -28,24 +84,6 @@ namespace CsvTools.Tests
         Assert.AreEqual("CET", testValue.TZ);
 
         test.Close();
-      }
-    }
-
-    [TestMethod()]
-    public async Task PagedFileReaderNextPage()
-    {
-      using (var process = new CustomProcessDisplay(UnitTestInitializeCsv.Token))
-      using (var reader = new CsvFileReader(setting.FullPath, setting.CodePageId, setting.SkipRows, setting.HasFieldHeader, setting.ColumnCollection, setting.TrimmingOption, setting.FileFormat.FieldDelimiter, setting.FileFormat.FieldQualifier, setting.FileFormat.EscapeCharacter, setting.RecordLimit, setting.AllowRowCombining, setting.FileFormat.AlternateQuoting, setting.FileFormat.CommentLine, setting.NumWarnings, setting.FileFormat.DuplicateQuotingToEscape, setting.FileFormat.NewLinePlaceholder, setting.FileFormat.DelimiterPlaceholder, setting.FileFormat.QuotePlaceholder, setting.SkipDuplicateHeader, setting.TreatLFAsSpace, setting.TreatUnknownCharacterAsSpace, setting.TryToSolveMoreColumns, setting.WarnDelimiterInValue, setting.WarnLineFeed, setting.WarnNBSP, setting.WarnQuotes, setting.WarnUnknownCharacter, setting.WarnEmptyTailingColumns, setting.TreatNBSPAsSpace, setting.TreatTextAsNull, setting.SkipEmptyLines, setting.ConsecutiveEmptyRows, setting.IdentifierInContainer, process))
-      {
-        var test = new PagedFileReader(reader, 5, UnitTestInitializeCsv.Token);
-        await test.OpenAsync(true, true, true, true);
-        Assert.AreEqual(5, test.Count);
-
-        bool collectionChangedCalled = false;
-        test.CollectionChanged += (o, s) => { collectionChangedCalled = true; };
-        await test.MoveToNextPageAsync();
-        Assert.IsTrue(collectionChangedCalled);
-        Assert.AreEqual(5, test.Count);
       }
     }
   }
