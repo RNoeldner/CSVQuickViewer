f9b9461b56a08faa3a572b5b60f08d81e6e58c2d
Raphael Noeldner <r_n@gmx.net>
2021-09-22 08:30:20 +0200

    Update of NeutralResourcesLanguage Change of Build Number

--- Library/SharedAssemblyInfo.cs
+++ Library/SharedAssemblyInfo.cs
@@ -11,6 +11,6 @@ using System.Reflection;
 // Version information for an assembly consists of the following four values: Major Version Minor
 // Version Build Number Revision You can specify all the values or you can default the Build and
 // Revision Numbers by using the '*' as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.15.501")]
-[assembly: AssemblyFileVersion("1.6.15.501")]
-[assembly: AssemblyInformationalVersion("1.6.15.501")] // a.k.a. "Product version"
+[assembly: AssemblyVersion("1.6.15.502")]
+[assembly: AssemblyFileVersion("1.6.15.502")]
+[assembly: AssemblyInformationalVersion("1.6.15.502")] // a.k.a. "Product version"
