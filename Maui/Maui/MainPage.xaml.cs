8ba0c6a57b5558ec0de8f2aa36739aaf3c75ccfa
Raphael Noeldner <r_n@gmx.net>
2022-06-08 13:55:28 +0200

    Added Maui Application Project

---
+++ Maui/Maui/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace Maui
+{
+  public partial class MainPage : ContentPage
+  {
+
+    public MainPage()
+    {
+      InitializeComponent();
+    }
+
+    private async void OpenFile_ClickedAsync(object sender, EventArgs e)
+    {
+      var options = new PickOptions()
+      {
+        PickerTitle="Deleimted Text Files",
+        FileTypes=  new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+          {
+            { DevicePlatform.iOS, new[] { "public.my.comic.extension" } },
+            { DevicePlatform.Android, new[] { "text/csv", "text/plain" } },
+            { DevicePlatform.WinUI, new[] { "csv", "tab" , "txt"} },
+            { DevicePlatform.Tizen, new[] { "*/*" } },
+            { DevicePlatform.macOS, new[] { "csv", "tab" , "txt"} } })
+      };
+      var result = await FilePicker.Default.PickAsync(options);
+      if (result != null)
+      {
+        OpenFile.Text = result.FileName;
+      }
+    }
+  }
+}
