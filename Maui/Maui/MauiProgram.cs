ed3ad385fd553b27fb693d1e399bf26144c677dc
Raphael Noeldner <r_n@gmx.net>
2022-06-08 13:55:28 +0200

    Added Maui Application Project

---
+++ Maui/Maui/MauiProgram.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Maui;
+
+namespace Maui
+{
+  public static class MauiProgram
+  {
+    public static MauiApp CreateMauiApp()
+    {
+      var builder = MauiApp.CreateBuilder();
+      builder
+        .UseMauiApp<App>()
+        .ConfigureFonts(fonts =>
+        {
+          fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+          fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+        });
+      // Initialise the toolkit
+      builder.UseMauiApp<App>().UseMauiCommunityToolkit();
+      return builder.Build();
+    }
+  }
+}
