44d6d9cd3cf39a009392d530fbeaff7373db417a
Raphael Noeldner <r_n@gmx.net>
2022-06-24 17:12:12 +0200

    updating BaseSettingPhysicalFile

--- Library/ClassLibraryCSV/Mutable/NotifyPropertyChangedBase.cs
+++ Library/ClassLibraryCSV/Mutable/NotifyPropertyChangedBase.cs
@@ -1,31 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
 namespace CsvTools
 {
-  /// <summary>
-  /// Provides data for the <see langword='PropertyChanged'/> event.
-  /// </summary>
-  public sealed class PropertyChangedStringEventArgs : PropertyChangedEventArgs
-  {
-    /// <summary>
-    /// Initializes a new instance of the <see cref='System.ComponentModel.PropertyChangedEventArgs'/>
-    /// class.
-    /// </summary>
-    public PropertyChangedStringEventArgs(string propertyName, string? oldValue, string? newValue) : base(propertyName)
-    {
-      OldValue = oldValue;
-      NewValue = newValue;
-    }
-
-    public string? OldValue { get; }
-    public string? NewValue { get; }
-  }
-
   public abstract class NotifyPropertyChangedBase : INotifyPropertyChanged
   {
     /// <inheritdoc />
@@ -37,7 +17,7 @@ namespace CsvTools
     public event EventHandler<PropertyChangedStringEventArgs>? PropertyChangedString;
 
     /// <summary>
-    ///   Notifies the completed property changed through <see cref="PropertyChanged"/>
+    ///   Notifies the completed property changed through <see cref="PropertyChanged" />
     /// </summary>
     /// <param name="propertyName">The property name.</param>
     protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
@@ -61,14 +41,14 @@ namespace CsvTools
     /// <param name="propertyName">The property name.</param>
     /// <param name="oldValue">The old value.</param>
     /// <param name="newVal">The new value.</param>
-    protected void NotifyPropertyChangedString(in string propertyName, in string oldValue, in string newVal)
+    protected void NotifyPropertyChangedString(in string propertyName, in string oldValue, in string newValue)
     {
       if (PropertyChangedString is null)
         return;
       try
       {
         // ReSharper disable once PolymorphicFieldLikeEventInvocation
-        PropertyChangedString(this, new PropertyChangedStringEventArgs(propertyName, oldValue, newVal));
+        PropertyChangedString(this, new PropertyChangedStringEventArgs(propertyName, oldValue, newValue));
       }
       catch (TargetInvocationException)
       {
@@ -77,13 +57,16 @@ namespace CsvTools
     }
 
     /// <summary>
-    /// Sets the  collection of a backing store and raises <see cref="PropertyChanged"/> after the value is changed
+    ///   Sets the collection of a backing store and raises <see cref="PropertyChanged" /> after the
+    ///   value is changed
     /// </summary>
     /// <param name="field">The backing store.</param>
     /// <param name="value">The new collection.</param>
-    /// <param name="withOrder">if <c>true</c> the order of the collections is important and will indicate a change</param>
+    /// <param name="withOrder">
+    ///   if <c>true</c> the order of the collections is important and will indicate a change
+    /// </param>
     /// <param name="propertyName">Name of the property.</param>
-    /// <returns><c>true></c> if the value was changed</returns>
+    /// <returns><c>true&gt;</c> if the value was changed</returns>
     protected bool SetCollection<T>(ICollection<T> field, in IEnumerable<T>? value, bool withOrder = true, [CallerMemberName] string propertyName = "") where T : IEquatable<T>
     {
       if (withOrder && field.CollectionEqualWithOrder(value))
@@ -107,12 +90,13 @@ namespace CsvTools
     }
 
     /// <summary>
-    /// Sets the property of a backing store and raises <see cref="PropertyChanged"/> after the value is changed
+    ///   Sets the property of a backing store and raises <see cref="PropertyChanged" /> after the
+    ///   value is changed
     /// </summary>
     /// <param name="field">The backing store.</param>
     /// <param name="value">The new value.</param>
     /// <param name="propertyName">Name of the property.</param>
-    /// <returns><c>true></c> if teh value was changed</returns>
+    /// <returns><c>true&gt;</c> if teh value was changed</returns>
     protected bool SetField<T>(ref T field, in T value, [CallerMemberName] string propertyName = "") // where T : struct
     {
       if (EqualityComparer<T>.Default.Equals(field, value))
@@ -123,44 +107,20 @@ namespace CsvTools
     }
 
     /// <summary>
-    /// Sets the observable collection of a backing store and raises <see cref="PropertyChanged"/> after the value is changed
-    /// </summary>
-    /// <param name="field">The backing store.</param>
-    /// <param name="value">The new collection.</param>
-    /// <param name="withOrder">if <c>true</c> the order of the collections is important and will indicate a change</param>
-    /// <param name="propertyName">Name of the property.</param>
-    /// <returns><c>true></c> if the value was changed</returns>
-    protected bool SetCollection<T>(ObservableCollection<T> field, in IEnumerable<T>? value, bool withOrder = true, [CallerMemberName] string propertyName = "") where T : IEquatable<T>
-    {
-      if (withOrder && field.CollectionEqualWithOrder(value))
-        return false;
-      if (!withOrder && field.CollectionEqual(value))
-        return false;
-
-      field.Clear();
-      if (value != null)
-      {
-        foreach (var item in value)
-        {
-          if (item is ICloneable cloneable)
-            field.Add((T) cloneable.Clone());
-          else
-            field.Add(item);
-        }
-      }
-      NotifyPropertyChanged(propertyName);
-      return true;
-    }
-
-    /// <summary>
-    /// Sets the string of a backing store and raises <see cref="PropertyChanged"/> after the value is changed
+    ///   Sets the string of a backing store and raises <see cref="PropertyChanged" /> after the
+    ///   value is changed
     /// </summary>
     /// <param name="field">The backing store.</param>
     /// <param name="value">The new value.</param>
-    /// <param name="comparison">Specifies the culture, case, and sort rules to be used for comparison, use <see cref="StringComparison.OrdinalIgnoreCase"/> if not sure</param>
-    /// <param name="notifyChange">If <c>true</c> the <see cref="PropertyChangedString"/> event is raised before the data is updated</param>
+    /// <param name="comparison">
+    ///   Specifies the culture, case, and sort rules to be used for comparison, use <see
+    ///   cref="StringComparison.OrdinalIgnoreCase" /> if not sure
+    /// </param>
+    /// <param name="notifyChange">
+    ///   If <c>true</c> the <see cref="PropertyChangedString" /> event is raised before the data is updated
+    /// </param>
     /// <param name="propertyName">Name of the property.</param>
-    /// <returns><c>true></c> if the value was changed</returns>
+    /// <returns><c>true</c> if the value was changed</returns>
     protected bool SetField(ref string field, in string? value, StringComparison comparison, bool notifyChange = false, [CallerMemberName] string propertyName = "")
     {
       var newValue = value ?? string.Empty;
