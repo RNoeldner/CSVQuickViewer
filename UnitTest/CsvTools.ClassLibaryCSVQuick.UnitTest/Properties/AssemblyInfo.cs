7077c356f4094c9d852e78f959db8b2f0b6cd6bb
Raphael Noeldner <r_n@gmx.net>
2022-09-15 10:40:16 +0200

    Moved two Classes into Quick Project for MAUI, and added new UnitTest Project Removed Foldy as it results in Virus warnings, and adjusted Setup Added new MSI setup for .NET6 Version

---
+++ UnitTest/CsvTools.ClassLibaryCSVQuick.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CsvTools.ClassLibaryCSVQuick.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CsvTools.ClassLibaryCSVQuick.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6e3bb97e-4b08-43e6-be66-b2b0063c1659")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
