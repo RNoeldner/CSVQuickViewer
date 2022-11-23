6e33d056953eed91b31966a61d984388ee43594d
Raphael Noeldner <r_n@gmx.net>
2022-11-23 11:19:25 +0100

    Fixing  Column and Mapping JsonConstructor; Fixed naming for immuatbel and mutable SampleDataCreationField

--- Library/ClassLibraryCSV/Column/WriterColumn.cs
+++ Library/ClassLibraryCSV/Column/WriterColumn.cs
@@ -15,7 +15,7 @@
 namespace CsvTools
 {
   /// <inheritdoc />
-  public class WriterColumn : Column
+  public sealed class WriterColumn : Column
   {
     public WriterColumn(
       in string name,
