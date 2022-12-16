d617314ced8107c8991145c5db4337fe5553df34
Raphael Noeldner <r_n@gmx.net>
2022-12-16 18:51:51 +0100

    ResizeForm Form not working in .NET 7 as resousure was not embedded

--- Library/WinFormControls/ResizeForm.cs
+++ Library/WinFormControls/ResizeForm.cs
@@ -5,63 +5,6 @@ using System.Windows.Forms;
 
 namespace CsvTools
 {
-  public class ResizeUserControl : UserControl
-  {
-    private IFontConfig? m_FontConfig;
-
-    [Browsable(false)]
-    [Bindable(false)]
-    public IFontConfig FontConfig
-    {
-      get => m_FontConfig;
-      set
-      {
-        if (m_FontConfig != null)
-          m_FontConfig.PropertyChanged -= FontSettingChanged;
-        m_FontConfig = value;
-#pragma warning disable CA1416
-        ChangeFont(new Font(m_FontConfig.Font, m_FontConfig.FontSize));
-#pragma warning restore CA1416
-        m_FontConfig.PropertyChanged += FontSettingChanged;
-      }
-    }
-
-    private void FontSettingChanged(object? sender, PropertyChangedEventArgs e)
-    {
-      if (m_FontConfig != null && e.PropertyName is nameof(IFontConfig.Font) or nameof(IFontConfig.FontSize))
-#pragma warning disable CA1416
-        ChangeFont(new Font(m_FontConfig.Font, m_FontConfig.FontSize));
-#pragma warning restore CA1416
-    }
-
-    public void ChangeFont(Font newFont)
-    {
-      this.SafeInvoke(() =>
-      {
-        SuspendLayout();
-        SetFonts(this, newFont);
-        ResumeLayout();
-        Refresh();
-      });
-    }
-
-    public void SetFont(Font newFont) =>
-      SetFonts(this, newFont);
-
-    /// <summary>
-    ///   Recursively change the font of all controls, needed on Windows 8 / 2012
-    /// </summary>
-    /// <param name="container">A container control like a form or panel</param>
-    /// <param name="newFont">The font with size to use</param>
-    private static void SetFonts(in Control container, in Font newFont)
-    {
-      if (!Equals(container.Font, newFont))
-        container.Font = newFont;
-
-      foreach (Control ctrl in container.Controls)
-        SetFonts(ctrl, newFont);
-    }
-  }
 
   public class ResizeForm : Form
   {
