4d9f1a613f986c8c5d46d20e9e50f90db36923c2
Raphael Noeldner <r_n@gmx.net>
2023-01-22 14:14:46 +0100

    Fixed renaming for MAUi project

--- Maui/Maui/MauiStatic.cs
+++ Maui/Maui/MauiStatic.cs
@@ -4,17 +4,12 @@ namespace Maui
 {
   public static class MauiStatic
   {
-    public static async Task<DetectionResult> GetDetectionResult(this string fullPath, CancellationToken cancellationToken)
+    public static async Task<InspectionResult> GetDetectionResult(this string fullPath, CancellationToken cancellationToken)
     {
       var preference = new PreferenceViewModel();
 
-      return await fullPath.AnalyzeFileAsync(false, preference.GuessCodePage,
-        preference.GuessEscapePrefix,
-        preference.GuessDelimiter,
-        preference.GuessQualifier,
-        preference.GuessStartRow,
-        preference.GuessHasHeader,
-        false,
+      return await fullPath.InspectFileAsync(false, preference.GuessCodePage,
+        preference.GuessEscapePrefix, preference.GuessDelimiter, preference.GuessQualifier, preference.GuessStartRow, preference.GuessHasHeader, false,
         preference.GuessComment, preference.GetFillGuessSettings(), cancellationToken).ConfigureAwait(false);
     }
   }
