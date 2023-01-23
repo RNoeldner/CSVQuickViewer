12e726f576e9677fc0aade74b88ccda47fd551a8
Raphael Noeldner <r_n@gmx.net>
2023-01-23 10:26:08 +0100

    Using netstandard2.1 for CsvTools.ClassLibraryValidator

--- Maui/Maui/MainPage.xaml.cs
+++ Maui/Maui/MainPage.xaml.cs
@@ -7,24 +7,6 @@ namespace Maui
     public MainPage()
     {
       InitializeComponent();
-    }
-
-    private async void MainPage_OnLoaded(object? sender, EventArgs e)
-    {
-      //if (!m_IsHandled)
-      //{
-      //  var goodArgs = AppInstance.GetCurrent().GetActivatedEventArgs();
-
-      //  if (goodArgs.Kind == ExtendedActivationKind.File && goodArgs.Data is IFileActivatedEventArgs fileActivatedEventArgs)
-      //  {
-      //    var fn = fileActivatedEventArgs.Select(file => file.Path).First();
-      //    if (FileSystemUtils.FileExists(fn))
-      //      // this is started but not awaited
-      //      await ViewModel.OpenAsync(fn);
-      //  }
-      //  m_IsHandled = true;
-      //}
-    }
-
+    }  
   }
 }
