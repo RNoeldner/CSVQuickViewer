60868a67878110dc376fd3bac76a062eeaf38dfb
Raphael Noeldner <r_n@gmx.net>
2023-02-03 09:32:57 +0100

    Updated Version Number for Realease

--- Library/SharedAssemblyInfo.cs
+++ Library/SharedAssemblyInfo.cs
@@ -11,6 +11,6 @@ using System.Reflection;
 // Version information for an assembly consists of the following four values: Major Version Minor
 // Version Build Number Revision You can specify all the values or you can default the Build and.Json
 // Revision Numbers by using the '*' as shown belokw: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.2.569")]
-[assembly: AssemblyFileVersion("1.7.2.569")]
-[assembly: AssemblyInformationalVersion("1.7.2.569")] // a.k.a. "Product version"
+[assembly: AssemblyVersion("1.7.2.570")]
+[assembly: AssemblyFileVersion("1.7.2.570")]
+[assembly: AssemblyInformationalVersion("1.7.2.570")] // a.k.a. "Product version"
