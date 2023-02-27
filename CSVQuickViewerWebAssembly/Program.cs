bb967a18a3d8f3be91ee4472e9b36e4e97bdc278
Raphael Noeldner <r_n@gmx.net>
2023-02-27 09:32:13 +0100

    Revert "Moving ViewSettings to Libary"
    This reverts commit c14ee7c6f91d85d7e7b67af273ecc0b1f64d836e.

--- CSVQuickViewerWebAssembly/Program.cs
+++ CSVQuickViewerWebAssembly/Program.cs
@@ -1,7 +1,6 @@
 using CSVQuickViewerWebAssembly;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Syncfusion.Blazor;
 
 internal class Program
 {
@@ -10,11 +9,11 @@ internal class Program
     var builder = WebAssemblyHostBuilder.CreateDefault(args);
     builder.RootComponents.Add<App>("#app");
     builder.RootComponents.Add<HeadOutlet>("head::after");
-    
+
     builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-    builder.Services.AddSyncfusionBlazor();
 
     var app = builder.Build();
+    
     await app.RunAsync();
   }
 }
