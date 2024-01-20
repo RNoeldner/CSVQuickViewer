fb45220dcc049f8640da5b14ef7e4290a1fd704c
Raphael Noeldner <r_n@gmx.net>
2024-01-20 13:00:26 +0100

    Better FileReaderWriterFactory class based on ClassLibraryCsvFileReaderWriterFactory

--- Library/ClassLibraryCSV/FileReader/ClassLibraryCsvFileReaderWriterFactory.cs
+++ Library/ClassLibraryCSV/FileReader/ClassLibraryCsvFileReaderWriterFactory.cs
@@ -23,16 +23,29 @@ namespace CsvTools
   /// <inheritdoc />
   public class ClassLibraryCsvFileReaderWriterFactory : IFileReaderWriterFactory
   {
-    protected readonly TimeZoneChangeDelegate m_TimeZoneAdjust;
-    protected readonly FillGuessSettings m_FillGuessSettings;
+    /// <summary>
+    /// Routine that will do time zone conversions
+    /// </summary>
+    protected readonly TimeZoneChangeDelegate TimeZoneAdjust;
+
+    /// <summary>
+    /// Setting determining which value types are determined
+    /// </summary>
+    protected readonly FillGuessSettings FillGuessSettings;
+
+    /// <summary>
+    /// While reading converting to this timezone (if a timezone field is defined)
+    /// While writing the timezone that is assumed for columns with time
+    /// </summary>
+    protected string LocalTimeZone = TimeZoneInfo.Local.Id;
 
     /// <summary>Initializes a new instance of the <see cref="ClassLibraryCsvFileReaderWriterFactory" /> class.</summary>
     /// <param name="timeZoneAdjust">The routine to do time zone adjustments</param>
     /// <param name="fillGuessSettings">The guess settings</param>
     public ClassLibraryCsvFileReaderWriterFactory(TimeZoneChangeDelegate timeZoneAdjust, FillGuessSettings fillGuessSettings)
     {
-      m_TimeZoneAdjust = timeZoneAdjust;
-      m_FillGuessSettings = fillGuessSettings;
+      TimeZoneAdjust = timeZoneAdjust;
+      FillGuessSettings = fillGuessSettings;
     }
 
     /// <inheritdoc />
@@ -40,10 +53,11 @@ namespace CsvTools
     {
       return setting switch
       {
-        IJsonFile json => new JsonFileReader(json.FullPath, json.ColumnCollection, json.RecordLimit, json.Trim, json.TreatTextAsNull, json.TreatNBSPAsSpace, m_TimeZoneAdjust,
-          TimeZoneInfo.Local.Id, false, false),
+        IJsonFile json => new JsonFileReader(json.FullPath, json.ColumnCollection, json.RecordLimit, json.Trim, json.TreatTextAsNull, json.TreatNBSPAsSpace, TimeZoneAdjust,
+          LocalTimeZone, FillGuessSettings.DetectPercentage, FillGuessSettings.RemoveCurrencySymbols),
 
-        IXmlFile xml => new XmlFileReader(xml.FullPath, xml.ColumnCollection, xml.RecordLimit, xml.Trim, xml.TreatTextAsNull, xml.TreatNBSPAsSpace, m_TimeZoneAdjust, TimeZoneInfo.Local.Id, false, false),
+        IXmlFile xml => new XmlFileReader(xml.FullPath, xml.ColumnCollection, xml.RecordLimit, xml.Trim, xml.TreatTextAsNull, xml.TreatNBSPAsSpace, TimeZoneAdjust,
+          LocalTimeZone, FillGuessSettings.DetectPercentage, FillGuessSettings.RemoveCurrencySymbols),
 
         ICsvFile csv => new CsvFileReader(csv.FullPath, csv.CodePageId, csv.SkipRows, csv.HasFieldHeader,
           csv.ColumnCollection, csv.TrimmingOption, csv.FieldDelimiterChar, csv.FieldQualifierChar, csv.EscapePrefixChar,
@@ -52,9 +66,11 @@ namespace CsvTools
           csv.QualifierPlaceholder, csv.SkipDuplicateHeader, csv.TreatLfAsSpace, csv.TreatUnknownCharacterAsSpace,
           csv.TryToSolveMoreColumns, csv.WarnDelimiterInValue, csv.WarnLineFeed, csv.WarnNBSP, csv.WarnQuotes,
           csv.WarnUnknownCharacter, csv.WarnEmptyTailingColumns, csv.TreatNBSPAsSpace, csv.TreatTextAsNull,
-          csv.SkipEmptyLines, csv.ConsecutiveEmptyRows, csv.IdentifierInContainer, m_TimeZoneAdjust,
-          TimeZoneInfo.Local.Id, m_FillGuessSettings.DetectPercentage, m_FillGuessSettings.RemoveCurrencySymbols),
-        _ => throw new FileReaderException($"Reader for {setting} not found")
+          csv.SkipEmptyLines, csv.ConsecutiveEmptyRows, csv.IdentifierInContainer, TimeZoneAdjust,
+          LocalTimeZone, FillGuessSettings.DetectPercentage, FillGuessSettings.RemoveCurrencySymbols),
+
+        _ => throw new FileReaderException($"Reader for {setting} not found"),
+
       };
     }
 
@@ -69,18 +85,18 @@ namespace CsvTools
           csv.ByteOrderMark, csv.ColumnCollection, csv.IdentifierInContainer, csv.Header,
           csv.Footer, csv.GetDisplay(), csv.NewLine, csv.FieldDelimiterChar, csv.FieldQualifierChar, csv.EscapePrefixChar,
           csv.NewLinePlaceholder, csv.DelimiterPlaceholder, csv.QualifierPlaceholder, csv.QualifyAlways,
-          csv.QualifyOnlyIfNeeded, csv.WriteFixedLength, m_TimeZoneAdjust, TimeZoneInfo.Local.Id, publicKey, csv.KeepUnencrypted
+          csv.QualifyOnlyIfNeeded, csv.WriteFixedLength, TimeZoneAdjust, LocalTimeZone, publicKey, csv.KeepUnencrypted
           ),
 
         IJsonFile jsonFile => new JsonFileWriter(jsonFile.FullPath, jsonFile.IdentifierInContainer,
           jsonFile.Footer, jsonFile.Header, jsonFile.EmptyAsNull, jsonFile.CodePageId,
           jsonFile.ByteOrderMark, jsonFile.ColumnCollection, jsonFile.GetDisplay(), jsonFile.Row,
-          m_TimeZoneAdjust, TimeZoneInfo.Local.Id, publicKey, jsonFile.KeepUnencrypted
+          TimeZoneAdjust, LocalTimeZone, publicKey, jsonFile.KeepUnencrypted
           ),
 
         IXmlFile xmlFile => new XmlFileWriter(xmlFile.FullPath, xmlFile.IdentifierInContainer, xmlFile.Footer,
           xmlFile.Header, xmlFile.CodePageId, xmlFile.ByteOrderMark, xmlFile.ColumnCollection, xmlFile.GetDisplay(),
-          xmlFile.Row, m_TimeZoneAdjust, TimeZoneInfo.Local.Id, publicKey, xmlFile.KeepUnencrypted
+          xmlFile.Row, TimeZoneAdjust, LocalTimeZone, publicKey, xmlFile.KeepUnencrypted
           ),
         _ => throw new FileWriterException($"Writer for {fileSetting} not found"),
       };
