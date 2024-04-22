8a232c37eaf8f45f3b2644006091c0eecddb61e8
Raphael Noeldner <r_n@gmx.net>
2024-04-22 10:24:22 +0200

    Improving Partitioning,

--- Application/Properties/SharedAssemblyInfo.cs
+++ Application/Properties/SharedAssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Reflection;
 // Version information for an assembly consists of the following four values: Major Version Minor
 // Version Build Number Revision You can specify all the values or you can default the Build and.Json
 // Revision Numbers by using the '*' as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.11.611")]
-[assembly: AssemblyFileVersion("1.7.11.611")]
-[assembly: AssemblyInformationalVersion("1.7.11.611")] // a.k.a. "Product version"
+[assembly: AssemblyVersion("1.7.11.612")]
+[assembly: AssemblyFileVersion("1.7.11.612")]
+[assembly: AssemblyInformationalVersion("1.7.11.612")] // a.k.a. "Product version"
